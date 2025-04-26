ffc7075e3ea5e9f418459488d2ac5c003fac9638
kamilnaja <kamilnaja@gmail.com>
2025-04-27 00:03:32 +0200

    adding chakra ui

---
+++ src/app/users/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Hello
+      </button>
+      <div>Hello</div>
+    </>
+  );
+}
